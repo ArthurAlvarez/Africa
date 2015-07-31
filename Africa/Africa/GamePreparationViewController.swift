eeee176bb184c8a41f28779351ca059c906c3706
Felipe Eulalio <felipedeulalio@gmail.com>
2015-07-31 13:47:47 -0300

    • outras melhorias no layout • Adicionados os pdfs e a leitura dos mesmos

---
+++ Africa/Africa/GamePreparationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GamePreparationViewController.swift
+//  
+//
+//  Created by Felipe Eulalio on 31/07/15.
+//
+//
+
+import UIKit
+
+class GamePreparationViewController: UIViewController {
+
+	@IBOutlet weak var webView: UIWebView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		let fileName = NSLocalizedString("gamePreparation", comment: "")
+		let path = NSBundle.mainBundle().pathForResource(fileName, ofType: "pdf")
+		let targetURL = NSURL(fileURLWithPath: path!)
+		let request = NSURLRequest(URL: targetURL!)
+		
+		self.webView.loadRequest(request)
+	}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Africa/GamePreparationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GamePreparationViewController.swift
+//  
+//
+//  Created by Felipe Eulalio on 31/07/15.
+//
+//
+
+import UIKit
+
+class GamePreparationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
